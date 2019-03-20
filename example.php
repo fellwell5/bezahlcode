6b8c30a83eb66e373cfd177db9262261c9ebce9f
Matthias Schaffer <matthias@poechinger.com>
2019-03-20 14:03:11 +0100

    Version 0.2
    Updated for Version 0.2 class

--- example.php
+++ example.php
@@ -1,15 +1,29 @@
 <?php
-  require "bezahlcode.class.php";
-	
+	require "bezahlcode.class.php";
+
 	$bezahlcode = new Bezahlcode("IBAN", "BIC", "bankaccount owner");
 	$bezahlcode->generatePayload("usage", "amount");
-	
+
 	/*	get Bezahlcode as base64-string
+	generateBase64(filetype)
+	filetype defaults to jpg; options: jpg, png, gif
 	----------------------------------- */
 	$base64 = $bezahlcode->generateBase64();
 	echo "<img src='$base64' alt='Bezahlcode' />";
-	
-	/*	save Bezahlcode as png
+
+	/*	save Bezahlcode
+	saveImage(filename, filetype)
+	filetype defaults to jpg; options: jpg, png, gif
 	----------------------------------- */
-	$bezahlcode->savePNG("output.png");
+	$bezahlcode->saveImage("output.jpg", "jpg");
+	// or
+	$bezahlcode->saveImage("output.jpg");
+	// or
+	$bezahlcode->saveImage("output.png", "png");
+
+	/*	output Bezahlcode to Webbrowser
+	outputImage(filetype)
+	filetype defaults to jpg; options: jpg, png, gif
+	----------------------------------- */
+	$bezahlcode->outputImage();
 ?>
